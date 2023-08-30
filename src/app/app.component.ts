c5e8544b9519af70895f2354e6dff777c63df06a
GOky01 <andrreyy7521@gmail.com>
2023-08-30 07:10:09 +0200

    add logic, html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'correnet';
+  fileToUpload: File | any;
+  imageUrls: string[] = [];
+  searchQuery: string = '';
+  ngOnInit() {
+    this.fetchImageUrls()
+  }
+  public imagesHtml = "";
+  constructor(private http: HttpClient) { }
+  onFileSelected(event: any) {
+    this.fileToUpload = event.target.files[0];
+  }
+  uploadImage() {
+    const formData: FormData = new FormData();
+    formData.append('file', this.fileToUpload, this.fileToUpload.name);
+
+    this.http.post<any>('http://localhost:8080/api/images/upload', formData)
+      .subscribe(
+      response => {
+        console.log(response);
+        if (response && response.message) {
+          this.fetchImageUrls();  // Fetch new URLs after successful upload
+        }
+      },
+      error => {
+        console.error('There was an error during the upload: ', error);
+      }
+    );
+
+  }
+
+  updateImagesHtml() {
+    this.imagesHtml = this.imageUrls.map(url => `<img src="${url}" alt="Uploaded Image">`).join('');
+    console.log(this.imagesHtml);
+  }
+  fetchImageUrls() {
+    this.http.get<string[]>('http://localhost:8080/api/images/urls')
+      .subscribe(urls => {
+        this.imageUrls = urls;
+        this.updateImagesHtml();
+      }, error => {
+        console.log('Error fetching image URLs:', error);
+      });
+  }
+
+  searchImages() {
+    this.http.get<string[]>(`http://localhost:8080/api/images/search?query=${this.searchQuery}`)
+      .subscribe(urls => {
+        this.imageUrls = urls;
+      }, error => {
+        console.log('Error fetching image URLs:', error);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
